19e26a649d0ace72a1d6e4b32b91461baff4767c
Kameron Bielawski <ktbielaw@vacc-user1.cluster>
2024-03-13 16:40:05 -0400

    grid searches

--- spacetime_grid_search.py
+++ spacetime_grid_search.py
@@ -7,32 +7,33 @@ param_template = {
     'n_trials': 1,
     'iters': 200,
     'dt': None,
+    'grid_res': None,
     'gravity': 5,
     'actuation_omega': 40, 
     'actuation_strength': 5,
-    'sim_body_size': None,
+    'sim_body_size': 0.1,
     'learning_rate': 30,
     'body_type': 'CILIA_BALL',
     'actuation_axes': [2]
 }
 
 # Grid search over timestep size (dt) and sim_body_size
-dt_values = np.linspace(5e-4, 5e-3, 10)
-sim_body_size_values = np.linspace(0.025, 0.25, 10)
+dt_values = np.linspace(0.0002,0.002,10)
+grid_res_values = np.linspace(10, 100, 10)
 
-print(dt_values, sim_body_size_values)
+print(dt_values, grid_res_values)
 
 # Create a new experiment file for each combination of dt and sim_body_size
 for dt in dt_values:
-    for sim_body_size in sim_body_size_values:
+    for grid_res in grid_res_values:
         param_template['dt'] = dt
-        param_template['sim_body_size'] = sim_body_size
-        param_template['name'] = f'grid_search_dt_{dt}_sim_body_size_{sim_body_size}'
+        param_template['grid_res'] = grid_res
+        param_template['name'] = f'grid_search_dt_{dt}_grid_res_{grid_res}'
 
         # Create experiment file
-        with open(f'dt_{dt}_sim_body_size_{sim_body_size}.exp', 'w') as exp_file:
+        with open(f'dt_{dt}_grid_res_{grid_res}.exp', 'w') as exp_file:
             exp_file.write(str(param_template) + '\n')
             
         # Run the experiment
         # This should launch a single trial with the specified parameters on the VACC
-        os.system(f'python run_exp.py --exp dt_{dt}_sim_body_size_{sim_body_size}.exp --vacc')
+        os.system(f'python run_exp.py --exp dt_{dt}_grid_res_{grid_res}.exp --vacc')
